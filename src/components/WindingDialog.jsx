eef01cd97f2494001969d6bd3c09911d789b8aa4
klillenb <kertlillenberk@gmail.com>
2022-06-14 00:02:14 +0300

    WindingDialog broken

--- src/components/WindingDialog.jsx
+++ src/components/WindingDialog.jsx
@@ -6,31 +6,72 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Paper, TableBody, TableCell, TableHead } from '@mui/material';
+import { Paper, TableContainer, TableCell, TableHead, TableRow, TableBody, Input } from '@mui/material';
 
-export default function WindingDialog(){
+// function createData(name, statorValue, rotorValue){
+//     return { name, statorValue, rotorValue };
+// }
 
-    // const useStyles = makeStyles(theme => ({
-    //     root: {
-    //       width: "100%",
-    //       marginTop: theme.spacing(3),
-    //       overflowX: "auto"
-    //     },
-    //     table: {
-    //       minWidth: 650
-    //     },
-    //     selectTableCell: {
-    //       width: 60
-    //     },
-    //     tableCell: {
-    //       width: 130,
-    //       height: 40
-    //     },
-    //     input: {
-    //       width: 130,
-    //       height: 40
-    //     }
-    // }));
+// const rows = [
+//     createData("Pooluste arv","-","-"),
+//     createData("Mähise liik","-","-"),
+//     createData("Ühendusviis","-","-"),
+//     createData("Mähise samm","-","-"),
+//     createData("Uurete arv","-","-"),
+//     createData("Keerdude arv","-","-"),
+//     createData("Raua mõõdud","-","-"),
+//     createData("Traatide arv keerus","-","-"),
+//     createData("Traadi mõõdud","-","-"),
+// ];
+const rows = [
+    {
+        name: "Pooluste arv",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Mähise liik",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Ühendusviis",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Mähise samm",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Uurete arv",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name:"Keerdude arv",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Raua mõõdud",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Traatide arv keerus",
+        statorValue: "",
+        rotorValue: ""
+    },
+    {
+        name: "Traadi mõõdud",
+        statorValue: "",
+        rotorValue: ""
+    }
+];
+
+export default function WindingDialog({ setsList = rows}){
 
     const [open, setOpen] = useState(false);
 
@@ -41,51 +82,22 @@ export default function WindingDialog(){
         setOpen(false);
     };
 
-    function createData(name, stator, rotor){
-        return { name, stator, rotor};
+    const [state, setState] = useState(setsList)
+    console.log(state)
+
+    const handleChange = (e, i) => {
+        const { value, name } = e.target;
+        console.log(value)
+
+        const newState = [...state];
+        newState[i] = {
+            ...newState[i],
+            [name]: value
+        };
+
+        //console.log(newState);
+        setState(newState);
     }
-
-    const CustomTableCell = ({ row, name, onChange }) => {
-        // const classes = useStyles();
-        return (
-        //   <TableCell align="left" className={classes.tableCell}>
-          <TableCell align="left" className={classes.tableCell}>
-              <Input
-                value={row[name]}
-                name={name}
-                onChange={e => onChange(e, row)}
-                // className={classes.input}
-              />
-          </TableCell>
-        );
-    };
-
-    const [rows, setRows] = useState([
-        createData("Pooluste arv", "-", "-"),
-        createData("Mähise liik", "-", "-"),
-        createData("Ühendusviis", "-", "-"),
-        createData("Mähise samm","-", "-"),
-        createData("Uurete arv", "-", "-"),
-        createData("Keerdude arv", "-", "-"),
-        createData("Raua mõõdud", "-", "-"),
-        createData("Traatide arv keerus", "-", "-"),
-        createData("Traadi mõõdud", "-", "-")
-    ])
-    //const classes = useStyles();
-
-    const onChange = (e, row) => {
-        const value = e.target.value;
-        const name = e.target.name;
-        const { id } = row;
-    
-        const newRows = rows.map(row => {
-            if (row.id === id) {
-                return { ...row, [name]: value };
-            }
-                return row;
-        });
-    }
-    
 
     return(
         <>
@@ -96,34 +108,43 @@ export default function WindingDialog(){
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle>Mähise andmed</DialogTitle>
                 <DialogContent>
-                    <Paper className={classes.root}>
-                        <Table className={classes.table} aria-label="caption table">
-                            <caption>Mähise andmed</caption>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align="left" />
-                                    <TableCell align="left">Staator</TableCell>
-                                    <TableCell align="left">Rootor</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {rows.map(row => (
-                                    <TableRow key={row.id}>
-                                        <TableCell className={classes.selectTableCell}></TableCell>
-                                        <CustomTableCell {...{ row, name: "Pooluste arv", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Mähise liik", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Ühendusviis", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Mähise samm", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Uurete arv", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Keerdude arv", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Raua mõõdud", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Traatide arv keerus", onChange}} />
-                                        <CustomTableCell {...{ row, name: "Traadi mõõdud", onChange}} />
+                    <TableContainer component={Paper}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="right"></TableCell>
+                                <TableCell align="left">Staator</TableCell>
+                                <TableCell align="left">Rootor</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {state.map(({ name, value }, index) => {
+                                return(
+                                    <TableRow
+                                        key={index}
+                                    >
+                                        <TableCell>
+                                            {name}
+                                            {": "}
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <TextField
+                                                name="statorValue"
+                                                value={value}
+                                                onChange={(e) => handleChange(e, index)}
+                                            />
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            <TextField
+                                                name="rotorValue"
+                                                value={value}
+                                                onChange={(e) => handleChange(e, index)}
+                                            />
+                                        </TableCell>
                                     </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </Paper>
+                                );
+                            })}
+                        </TableBody>
+                    </TableContainer>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Katkesta</Button>
