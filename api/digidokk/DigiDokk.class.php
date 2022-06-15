1f8b78c9bfc4c690d6071a23c162d25f7eaf77b3
Rain Erik Elias <90192521+RainErikE@users.noreply.github.com>
2022-06-15 12:04:28 +0300

    PHP "toimib"

--- api/digidokk/DigiDokk.class.php
+++ api/digidokk/DigiDokk.class.php
@@ -6,12 +6,11 @@
 class DigiDokk{
 
     private $return_data = null;
-
     public function get_data(){return $this->return_data;}
 
     public function query($id){$this->query_project($id);}
 
-    private function query_project($id){
+    private function query_project($id): void{
 
         //	TODO: see tööle panna..
         // session_start();
@@ -20,32 +19,31 @@ class DigiDokk{
         $this->return_data = null;
         $list_html = array();
 
+
+
 		$conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"], $GLOBALS["db_port"]);
 		$conn->set_charset("utf8");
-        $stmt = $conn->prepare("SELECT * FROM project WHERE id = ?");
+        $stmt = $conn->prepare("SELECT id, projekt_nr, prioriteet, alustatud, klient_id, kokkulepitud_lopp, lopp, valjaviidud, arve, saabunud, tagastus, teostatav, vottis_vastu, arhiivi FROM projekt WHERE id = ?");
         $stmt->bind_param("i", $id);
-        $stmt->bind_result($id_from_db, $projekt_nr_from_db, $prioriteet_from_db, $klient_id_from_db, $kokkulepitud_lopp_from_db, $lopp_from_db, $valjaviidud_from_db, $arve_from_db,
-            $saabunud_from_db, $tagastus_from_db, $teostatav_from_db, $vottis_vastu_from_db);
+         $stmt->bind_result($id2, $projekt_nr, $prioriteet, $alustatud, $klient_id, $kokkulepitud_lopp, $lopp, $valjaviidud, $arve, $saabunud, $tagastus, $teostatav, $vottis_vastu, $arhiivi);
         $stmt->execute();
         if($stmt->fetch()){
-            
-                array_push($list_html, array(
-                    "id"=>$id_from_db,
-                    "projekt_nr"=>$projekt_nr_from_db,
-                    "prioriteet"=>$prioriteet_from_db,
-                    "kliendi id"=>$klient_id_from_db,
-                    "kokkulepitud lopp"=>$kokkulepitud_lopp_from_db,
-                    "lopp"=>$lopp_from_db,
-                    "valjaviidud"=>$valjaviidud_from_db,
-                    "arve"=>$arve_from_db,
-                    "saabunud"=>$saabunud_from_db,
-                    "tagastus"=>$tagastus_from_db,
-                    "teostatav"=>$teostatav_from_db,
-                    "vottis vastu"=>$vottis_vastu_from_db,
-                    "notice"=>"Info edukalt saadud!",
-                    "type"=>"success"));
-         
-
+            array_push($list_html, array(
+                "id"=>$id2,
+                "projekt_nr"=>$projekt_nr,
+                "prioriteet"=>$prioriteet,
+                "kliendi_id"=>$klient_id,
+                "kokkulepitud_lopp"=>$kokkulepitud_lopp,
+                "lopp"=>$lopp,
+                "valjaviidud"=>$valjaviidud,
+                "arve"=>$arve,
+                "saabunud"=>$saabunud,
+                "tagastus"=>$tagastus,
+                "teostatav"=>$teostatav,
+                "vottis_vastu"=>$vottis_vastu,
+                "arhiivi"=>$arhiivi,
+                "notice"=>"Info edukalt saadud!",
+                "type"=>"success"));
         }else{
             array_push($list_html, array(
             "notice"=>"Infot ei saadud!",
@@ -61,9 +59,6 @@ class DigiDokk{
             array_push($list_html, array("notice"=>"Viga andmebaasiga suhtlemisel!", "type"=>"error"));
             $this->return_data = json_encode($list_html);
 		}
-        $this->return_data = json_encode($list_html);
-
-
     }
 
 }
--- api/digidokk/DigiDokk.php
+++ api/digidokk/DigiDokk.php
@@ -1,12 +1,14 @@
 <?php
+
 	header("Access-Control-Allow-Origin: *");
     require_once("DigiDokk.class.php");
 	if($_SERVER['REQUEST_METHOD'] != 'GET') {exit;}		// ! Kas on ikka GET päring?
-	$digidokk = new Digidokk();							// ! uus objekt
 
-    // * tabeli kohta infi küsimine
+	$DD = new DigiDokk();
+
+    //// * tabeli kohta infi küsimine
 	if(isset($_GET["id"]) and !empty($_GET["id"])){
 		$id = $_GET["id"];
-        $digidokk->query($id);
-        echo($digidokk->get_data());
+        $DD->query($id);
+        echo($DD->get_data());
 	}
