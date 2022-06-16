2375f7d4461dba55fb51e334fca6ad59b5e1fea8
Martin Lukas <90237322+DrSkyrim@users.noreply.github.com>
2022-06-16 13:48:29 +0300

    Uuendatud valikute vaartuste valiku fnc

--- api/choices/fnc_select_choices.php
+++ api/choices/fnc_select_choices.php
@@ -1,22 +1,42 @@
-<?php 
-
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Content-Type: *; charset=UTF-8");
-    require_once("../config.php");
-
-    read_for_table();
+<?php
+	require_once('../config.php');
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Headers: *");
+	header("Content-Type: *; charset=UTF-8");
 
 
-    function read_for_table(){
-        $choice_all_info_array = array();
+    get_device_info();
+
+
+    // ID järgi valiku nimetus
+    // 1 - liik
+    // 2 - võimsus kW
+    // 3 - p/min
+    // 4 - tootja
+    // 5 - võlli kõrgus
+    // 6 - toite liik
+    // 7 - sagedus Hz
+    // 8 - Isol. klass
+    // 9 - IP klass
+    // 10 - tunnihind
+    // 11 - takistuse ühik
+    // 12 - pingeteim
+    // 13 - katsetuse pinge(V)
+    // 14 - ühendus
+    // 15 - katsetatud
+
+    function get_device_info(){
+        $device_info_array = array();
         $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"], $GLOBALS["db_port"]);
         $conn->set_charset("utf8");
-        $stmt=$conn->prepare("SELECT valikute_vaartused.id,valikute_vaartused.vaartus,valikute_nimetus.nimetus FROM valikute_vaartused JOIN valikute_nimetus ON valikute_vaartused.valikute_nimetus_id=valikute_nimetus.id");
-        $stmt->bind_result($id_from_db,$name_from_db,$type_from_db);
+        $stmt = $conn->prepare("SELECT id, vaartus, valikute_nimetus_id FROM valikute_vaartused WHERE deleted='0'");
+        $stmt->bind_result($id_from_db, $device_info_from_db, $attribute_from_db);
         $stmt->execute();
         while($stmt->fetch()){
-            array_push($choice_all_info_array, array("id"=>$id_from_db, "name"=>$name_from_db, "type"=>$type_from_db));
+            array_push($device_info_array, array("id"=>$id_from_db, "attribute"=>$attribute_from_db, "name"=>$device_info_from_db));
         }
-        echo json_encode($choice_all_info_array);
+        echo json_encode($device_info_array);
+        $stmt->close();
+        $conn->close();
     }
+?>
