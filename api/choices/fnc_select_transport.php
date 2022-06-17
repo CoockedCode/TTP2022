56baf22d813d4e45a132c8e169a0f221ab8eea08
Martin Lukas <90237322+DrSkyrim@users.noreply.github.com>
2022-06-17 10:00:21 +0300

    Puhastan edasi

--- api/choices/fnc_select_transport.php
+++ api/choices/fnc_select_transport.php
@@ -4,20 +4,22 @@
 	header("Access-Control-Allow-Headers: *");
 	header("Content-Type: *; charset=UTF-8");
 
+    if(isset($_GET["transport"])){
+        get_transport();
+    }
 
-    get_transport_info();
-
-    function get_transport_info(){
+    function get_transport(){
         $transport_array = array();
         $conn = new mysqli($GLOBALS["server_host"], $GLOBALS["server_user_name"], $GLOBALS["server_password"], $GLOBALS["database"], $GLOBALS["db_port"]);
         $conn->set_charset("utf8");
-        $stmt = $conn->prepare("SELECT id,name FROM transpordi_firma WHERE deleted='0'");
-        $stmt->bind_result($id_from_db,$name_from_db);
+        $stmt = $conn->prepare("SELECT id, name FROM firma WHERE tuup = '1' AND deleted = '0'");
+        $stmt->bind_result($id_from_db, $name_from_db);
         $stmt->execute();
         while($stmt->fetch()){
-            array_push($transport_array, array("id"=>$id_from_db,"name"=>$name_from_db));
+            array_push($transport_array, array("id"=>$id_from_db, "name"=>$name_from_db));
         }
         echo json_encode($transport_array);
         $stmt->close();
         $conn->close();
     }
+?>
