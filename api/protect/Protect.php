d60f43a42f3c4ba93297cfe23700504686a8d2e9
Siim Kriibi <siim02@tlu.ee>
2022-06-17 13:36:31 +0300

    api fix

--- api/protect/Protect.php
+++ api/protect/Protect.php
@@ -1,6 +1,6 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
-
+	require_once("Protect.class.php");
 	if($_SERVER['REQUEST_METHOD'] != 'GET') {exit;}		// ! Kas on ikka GET päring?
 	$protect = new Protect();							// ! uus objekt
 
@@ -24,7 +24,6 @@
 		$id = $_GET["id"];
         $protect->query($table, $id);
         echo($protect->get_data());
-
 	}
 
 	// ! SHUDOWN
